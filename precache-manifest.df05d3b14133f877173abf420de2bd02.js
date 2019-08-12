dbdd2003bc6c752f4795bccda55b80d0877d3165
Angel Rivas <angel@prodigycode.com>
2019-08-11 23:30:08 -0500

    Updates

---
+++ precache-manifest.df05d3b14133f877173abf420de2bd02.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fb0a71d4ae242df91842ee018f66c54",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "c972a3b80282625311a1",
+    "url": "/Clicky-Game/static/css/main.a1fc2f22.chunk.css"
+  },
+  {
+    "revision": "3fec8b09d3e3e2136c47",
+    "url": "/Clicky-Game/static/js/2.1f15b8d5.chunk.js"
+  },
+  {
+    "revision": "c972a3b80282625311a1",
+    "url": "/Clicky-Game/static/js/main.f24879e0.chunk.js"
+  },
+  {
+    "revision": "cd703b7fefc761b0c13c",
+    "url": "/Clicky-Game/static/js/runtime~main.f4beb671.js"
+  },
+  {
+    "revision": "60a4e8679c2a06baed397394072972e6",
+    "url": "/Clicky-Game/static/media/Afrojack.60a4e867.jpg"
+  },
+  {
+    "revision": "0f9bbbc2e3d4ac6d230c049bcc639730",
+    "url": "/Clicky-Game/static/media/djSnake.0f9bbbc2.jpeg"
+  },
+  {
+    "revision": "24f6e2603bfa1185f4c2e89ba6ed4992",
+    "url": "/Clicky-Game/static/media/kaskade.24f6e260.jpg"
+  },
+  {
+    "revision": "06df69c8515e9641614460a99f53f98e",
+    "url": "/Clicky-Game/static/media/tiesto.06df69c8.jpeg"
+  }
+]);

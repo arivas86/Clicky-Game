56571c3ff341d282438cb28fc0ebf489fc04fef2
Angel Rivas <angel@prodigycode.com>
2019-08-11 21:48:02 -0500

    added nav component and cards

--- src/App.js
+++ src/App.js
@@ -1,26 +1,75 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Navbar from "./components/Navbar";
+import Cards from "./components/Cards";
+import ST from "./ST.json";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    ST,
+    clickedST: [],
+    score: 0
+  };
+  imageClick = event => {
+    const currentST = event.target.alt;
+    const STAlreadyClicked =
+      this.state.clickedST.indexOf(currentST) > -1;
+
+    if (STAlreadyClicked) {
+      this.setState({
+        ST: this.state.ST.sort(function (a, b) {
+          return 0.5 - Math.random();
+        }),
+        clickedST: [],
+        score: 0
+      });
+      alert("LOSER!");
+
+    } else {
+      this.setState(
+        {
+          ST: this.state.ST.sort(function (a, b) {
+            return 0.5 - Math.random();
+          }),
+          clickedST: this.state.clickedST.concat(
+            currentST
+          ),
+          score: this.state.score + 1
+        },
+        () => {
+          if (this.state.score === 12) {
+            alert("You Win!");
+            this.setState({
+              ST: this.state.ST.sort(function (a, b) {
+                return 0.5 - Math.random();
+              }),
+              clickedST: [],
+              score: 0
+            });
+          }
+        }
+      );
+    }
+  };
+
+  render() {
+    return (
+      <div>
+        <Navbar
+          score={this.state.score}
+        />
+        <div className="wrapper">
+          {this.state.ST.map(ST => (
+            <Cards
+              imageClick={this.imageClick}
+              id={ST.id}
+              key={ST.id}
+              image={ST.image}
+            />
+          ))}
+        </div>
+      </div>
+    );
+  }
 }
-
 export default App;
---
+++ src/components/navigation/Navbar.js
@@ -0,0 +1,18 @@
+
+import React, { Component } from "react";
+import "./Navbar.css";
+
+class Navbar extends Component {
+    render() {
+        return (
+            <nav className="navbar navbar-default navbar-fixed-top">
+                <h3 id="title">Stranger Things Memory game!</h3>
+                <p id="inst">Click an image to begin! Beware the Mindslayer if you select the same one twice!</p>Score: {this.props.score}
+            </nav>
+
+
+        );
+    }
+}
+
+export default Navbar;
